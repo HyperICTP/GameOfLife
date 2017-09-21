74690be3c46b36c8e96fc379a76c60f762fa1923
SoftwareEngineeringPractice2017 <31974523+SoftwareEngineeringPractice2017@users.noreply.github.com>
2017-09-21 19:18:07 +1000

    Add project files.

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Time to get this on OpenShift :D!" );
+            Console.ReadLine();
+        }
+    }
+}
